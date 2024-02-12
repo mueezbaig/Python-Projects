a294fb5dbe3db806bee7d6c69994481ed2b2194d
mueezbaig <moizzbaig613@gmail.com>
2024-02-12 18:52:26 +0530

    Initial commit

---
+++ Password_Manager/main.py
@@ -0,0 +1,26 @@
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            print(line.strip())
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + pwd + "\n")
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view/add), press q to quit? ").lower()
+
+    if mode == 'q':
+        break
+
+    if mode == 'view':
+        view()
+    
+    elif mode == 'add':
+        add()
+
+    else:
+        print('Invalid Mode')
